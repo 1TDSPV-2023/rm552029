d1298525a3e383d4512a6bfe12abc7a222494502
renatoatanes <rm552029@fiap.com.br>
2023-05-16 22:59:55 -0300

    js+login add

--- js/login.js
+++ js/login.js
@@ -1,30 +1,82 @@
 "use strict";
 
-//CRIANDO OBJETOS
-const usuario1 = {
-    nomeUsuario : "rita-lee",
-    senhaUsuario : "00000"
-}
-
-const usuario2 = {
-    nomeUsuario : "ptt01",
-    senhaUsuario : "12345"
-}
 
 //LISTA DE USUÁRIOS
-let listaDeUsuarios = [];
-listaDeUsuarios.push(usuario1);
-listaDeUsuarios.push(usuario2);
+let listaDeUsuarios = [
+
+ {
+   nomeCompleto:"jose das couves",
+   nomeusuario: "jose",
+   senhaUsuario : "123456"
+
+ }
+
+,
+
+{
+    nomeCompleto:"joao Paulino",
+    nomeusuario: "joao",
+    senhaUsuario : "123456"
+ 
+  }
+
+  ,
+ {
+    nomeCompleto:"Maria tomaite",
+    nomeusuario: "maria",
+    senhaUsuario : "123456"
+ 
+  }
+  ,
+
+  
+  {
+    nomeCompleto:"Paulo da Selva",
+    nomeusuario: "paulo",
+    senhaUsuario : "123456"
+ 
+  }
+
+];
+
+
+localStorage.setItem("listaUser",JSON.stringify (listaDeUsuarios));
+
+
 
 addEventListener("click", (evento)=>{
      
     let userInput = document.querySelector("#idUser"); 
     let passInput = document.querySelector("#idPass"); 
 
+     //MOSTRAR SENHA 
+        
+    //     const eyePass = document.querySelector(".fa-eye");
+
+          //MOSTRAR SENHA NO OLHINHO 
+
+         if(evento.target.className == "fa fa-eye"|| evento.target.className =="fa fa-eye-slash"){
+
+         evento.target.setAttribute("style", "cursor:pointer")
+
+         if(passInput.getAttribute("type")== "password"){
+            passInput.setAttribute("type","text");
+            evento.target.setAttribute("class","fa fa-eye-slash");
+            
+         }else{
+            evento.target.setAttribute("class", "cursor:pointer"); 
+            evento.target.setAttribute("class", "fa fa-eye");
+            passInput.setAttribute("type","password");
+           }
+
+         }
+
+         let listaDeUsuariosRecuperada = JSON.parse (localStorage.getItem("listUser"));
+         
         if(evento.target.id == "btnSubmit"){
         
         try{
-                listaDeUsuarios.forEach((usuario)=>{
+                listaDeUsuariosRecuperada.forEach((usuario)=>{
 
                     if(userInput.value == usuario.nomeUsuario && passInput.value == usuario.senhaUsuario){
                         throw "USUÁRIO VALIDADO!";
@@ -35,7 +87,9 @@ addEventListener("click", (evento)=>{
 
     }catch(msg){
         if(msg == "USUÁRIO VALIDADO!"){
-            console.log("USUÁRIO VALIDADO!")
+            //Redirect
+            window.location.hrer = "../js/sucesso.html";
+        
         }else{
             console.log("USUÁRIO OU SENHA INCORRETOS!");
         }
