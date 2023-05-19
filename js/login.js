85a23fe4abb4a95fd83f879eb5c8591a3c85be17
renatoatanes <RM552029@fiap.com.br>
2023-05-18 21:41:29 -0300

    js ajustado

--- js/login.js
+++ js/login.js
@@ -1,99 +1,81 @@
-"use strict";
 
 
 //LISTA DE USUÁRIOS
 let listaDeUsuarios = [
 
- {
-   nomeCompleto:"jose das couves",
-   nomeusuario: "jose",
-   senhaUsuario : "123456"
-
- }
-
-,
-
-{
-    nomeCompleto:"joao Paulino",
-    nomeusuario: "joao",
-    senhaUsuario : "123456"
- 
-  }
-
-  ,
- {
-    nomeCompleto:"Maria tomaite",
-    nomeusuario: "maria",
-    senhaUsuario : "123456"
- 
-  }
-  ,
-
-  
   {
-    nomeCompleto:"Paulo da Selva",
-    nomeusuario: "paulo",
-    senhaUsuario : "123456"
- 
+      nomeCompleto : "José das Couves",
+      nomeUsuario : "jose",
+      senhaUsuario : "123456"
   }
-
+  ,
+  {
+      nomeCompleto : "João Paulino",
+      nomeUsuario : "joao",
+      senhaUsuario : "123456"
+  }
+  ,
+  {
+      nomeCompleto : "Maria Tomaite",
+      nomeUsuario : "maria",
+      senhaUsuario : "123456"
+  }
+  ,
+  {
+      nomeCompleto : "Paulo da Selva",
+      nomeUsuario : "paulo",
+      senhaUsuario : "123456"
+  }
 ];
 
-
-localStorage.setItem("listaUser",JSON.stringify (listaDeUsuarios));
+localStorage.setItem("listaUser" ,JSON.stringify(listaDeUsuarios));
 
 
 
 addEventListener("click", (evento)=>{
-     
-    let userInput = document.querySelector("#idUser"); 
-    let passInput = document.querySelector("#idPass"); 
+   
+  let userInput = document.querySelector("#idUser"); 
+  let passInput = document.querySelector("#idPass"); 
+      
+      //MOSTRA SENHA NO OLHINHO
+      if(evento.target.className == "fa fa-eye" || evento.target.className == "fa fa-eye-slash"){
 
-     //MOSTRAR SENHA 
-        
-    //     const eyePass = document.querySelector(".fa-eye");
+          evento.target.setAttribute("style","cursor:pointer")
 
-          //MOSTRAR SENHA NO OLHINHO 
+          if(passInput.getAttribute("type") == "password"){
+              passInput.setAttribute("type","text");
+              evento.target.setAttribute("class","fa fa-eye-slash")  
+          }else{
+              evento.target.setAttribute("style","cursor:pointer");
+              evento.target.setAttribute("class","fa fa-eye");  
+              passInput.setAttribute("type","password"); 
+          }
 
-         if(evento.target.className == "fa fa-eye"|| evento.target.className =="fa fa-eye-slash"){
+      }
 
-         evento.target.setAttribute("style", "cursor:pointer")
+      let listaDeUsuariosRecuperada = JSON.parse(localStorage.getItem("listaUser"));
 
-         if(passInput.getAttribute("type")== "password"){
-            passInput.setAttribute("type","text");
-            evento.target.setAttribute("class","fa fa-eye-slash");
-            
-         }else{
-            evento.target.setAttribute("class", "cursor:pointer"); 
-            evento.target.setAttribute("class", "fa fa-eye");
-            passInput.setAttribute("type","password");
-           }
+      if(evento.target.id == "btnSubmit"){
+      
+      try{
+              listaDeUsuariosRecuperada.forEach((usuario)=>{
 
-         }
+                  if(userInput.value == usuario.nomeUsuario && passInput.value == usuario.senhaUsuario){
+                      throw "USUÁRIO VALIDADO!";
+                  }
+              });
 
-         let listaDeUsuariosRecuperada = JSON.parse (localStorage.getItem("listUser"));
-         
-        if(evento.target.id == "btnSubmit"){
-        
-        try{
-                listaDeUsuariosRecuperada.forEach((usuario)=>{
+              throw "USUÁRIO OU SENHA INCORRETOS!";
 
-                    if(userInput.value == usuario.nomeUsuario && passInput.value == usuario.senhaUsuario){
-                        throw "USUÁRIO VALIDADO!";
-                    }
-                });
+  }catch(msg){
+      if(msg == "USUÁRIO VALIDADO!"){
+          //Redirect
+          window.location.href = "../sucesso.html";
 
-                throw "USUÁRIO OU SENHA INCORRETOS!";
-
-    }catch(msg){
-        if(msg == "USUÁRIO VALIDADO!"){
-            //Redirect
-            window.location.hrer = "../js/sucesso.html";
-        
-        }else{
-            console.log("USUÁRIO OU SENHA INCORRETOS!");
-        }
-    }
-
+      }else{
+          console.log("USUÁRIO OU SENHA INCORRETOS!");
+      }
   }
+
+}
 });
